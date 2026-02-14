b156940ba6b309c646c255757b870fd9393dc790
HossamElnagar <Hossam7009@gmail.com>
2026-02-14 17:32:32 +0200

    phase one done

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ require("dotenv").config()
 
 //require express
 const express = require("express")
+//bcrypt
+const bcrypt = require("bcrypt")
 const mongoose = require("mongoose")
 
 
@@ -21,8 +23,81 @@ async function dbconnection(){
     }
 }
 
+dbconnection()
+
+const User = require("./models/user")
+
+
 //creating port
-const port = process.env.PORT ||3000
+const port = process.env.PORT || 3000;
+
+//routes
+app.post("/register",async(req,res)=>{
+    try {
+        
+        //get data
+        const {username,email, password,role} = req.body;
+        //validated data
+        if(!username || !email || !password) return res.status(400).json({msg:"Missing Data"});
+        // if exist user
+        const existuser = await  User.findOne({email});
+        if (existuser) return res.status(400).json({msg:"Account allready exist"});
+        //create user
+        const hashpassword = await bcrypt.hash(password, 10) //count of numer hashing
+
+        const user = await User.create({
+            username,
+            email,
+            password: hashpassword,
+            role
+        })
+        //response
+        res.status(201).json({
+            msg: "Done Created User",
+            data: user
+        })
+
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+
+
+
+
+/////////login
+
+app.post("/login",async(req,res)=>{
+    try {
+        //get data
+        const {email,password} = req.body;
+        //validation data
+        if(!email||!password) return res.status(400).json({msg:"missing data"})
+        //   
+        
+         const user = await  User.findOne({email});
+        if (!user) return res.status(400).json({msg:"Account not exist"});
+        const matchpassword = await bcrypt.compare(password, user.password) //count of numer hashing
+        if (!matchpassword)
+            return res.ststus(400).json({msg:"Invalid Passoword"});
+
+
+        const authCode = Buffer.from(user._id.toString()).toString("base64");
+
+
+        res.status(200).json({
+            msg: "success login",
+            data: authCode})
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+//////////////////////////////////////////product
+
+
+
 
 // port listening
 app.listen(port,()=>{
---
+++ instru.js
@@ -0,0 +1,12 @@
+//cereate user(email,password,role(admin,user))
+//login                  //phase 1
+
+
+//products
+//search product  //phase 2
+
+
+//cart             //phase 3
+
+
+//order /check   //phase 4
