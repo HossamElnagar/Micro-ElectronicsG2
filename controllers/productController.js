f09b0f3807e9062dc02b64581ec180b6eaaaba7f
Ahmed Maher <AhmedMa1her@gmail.com>
2026-02-21 14:45:36 +0200

    update and delete added

--- controllers/productController.js
+++ controllers/productController.js
@@ -32,7 +32,33 @@ const createProduct = async (req, res) => {
         res.status(500).json({ msg: "Server Error" });
     }
 };
+const updateProduct= async(req,res)=>{
+     try {
+            const {id} = req.params;
+            const product = await Product.findByIdAndUpdate(id, req.body, {new: true});
+            if(!product) return res.status(404).json({msg:"product not found"});
+            res.status(200).json({msg:"product updated", data: product})
+        } catch (error) {
+            console.log(error);
+            res.status(400).json({msg:"product update failed"})
+        }   
+}
+const deleteProduct= async (req,res) => {
+     try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndDelete(id);
+        if(!product) return res.status(404).json({msg:"product not found"});
+        res.status(200).json({msg:"product deleted"})
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({msg:"product deletion failed"})
+    }
+
+    
+}
 
 module.exports = {
-    createProduct
+    createProduct,
+    updateProduct,
+    deleteProduct
 };
