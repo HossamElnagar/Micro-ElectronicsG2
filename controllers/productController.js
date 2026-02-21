a41b5736e0fbd1b831bb15ef4d4b60464c60604f
HossamElnagar <Hossam@gmail.com>
2026-02-21 14:43:04 +0200

    first

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,1 +1,35 @@
-const Product = require("../models/Product")
+const Product = require("../models/Product");
+
+const createProduct = async (req, res) => {
+    try {
+
+        const { name, quantity, price } = req.body;
+
+        // validation
+        if (!name || !price)
+            return res.status(400).json({ msg: "missing required data" });
+
+        //user validate
+        if (req.user.role !== 'admin') {
+        return res.status(403).json({ msg: "Access denied: Admins only" });
+        }
+        const product = await Product.create({
+            name,
+            quantity,
+            price
+        });
+
+        res.status(201).json({
+            msg: "Product Created Successfully",
+            data: product
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ msg: "Server Error" });
+    }
+};
+
+module.exports = {
+    createProduct
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const { userRegister, userLogin} = require("../controllers/userController");
+const { createProduct } = require("../controllers/productController");
 
 router.post("/register",userRegister);
 
 router.post("/login",userLogin);
+router.post("/newProduct",createProduct);
 
 module.exports = router;
